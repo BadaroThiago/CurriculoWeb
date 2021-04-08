4bdcf6ed9ff6dc5651cb762c915fdfe32ecbec5f
BadaroThiago <tbadarodeoliveira@gmail.com>
2021-04-08 00:27:30 -0300

    Ajustes Textos

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function ExibeConhecimentos(){
                     <p class="card-text">
                         Granduação em Ciência da Computação pelo Uniceub - Em Andamento. <br>
                         Certificado Nível C1 ou Superior em Inglês pela Cambridge - 2020. <br>
-                        Curso Javascript: Programando na Linguagem da Web pela Alura - 2019 <br>
+                        Curso Javascript: Programando na Linguagem da Web pela Alura - 2019. <br>
                         Certificado em Gamificação pela Funifier - 2018. <br>
                         Participação em Treinamento de Scrum Ministrado pela K21 - 2017. <br> <br> <br> <br> <br>
                     </p>
@@ -80,20 +80,20 @@ function ExibeConquistas(){
         <div class="card-body">
             <h5 class="card-title">Conquistas:</h5>
             <p class="card-text">
-                Desenvolvimento de Módulos de Abertura de Conta Corrente PJ Digital 2021. <br>
-                Elaboração, Construção e Manutenção do Planeta BB – 2019 a 2021.<br>
-                Realização de 9 Workshops sobre Gamificação – 2018 a 2019.<br>
+                Desenvolvimento de Módulos de Abertura de Conta Corrente PJ Digital - 2021. <br>
+                Elaboração, Construção e Manutenção do Planeta BB - 2019 a 2021.<br>
+                Realização de 9 Workshops sobre Gamificação - 2018 a 2019.<br>
                 Convidado das edições 10 e 14 do Café com UX - 2018 e 2019.<br>
-                Elaboração e Manutenção do Talentos em Conexão – 2018 e 2019.<br>
-                Coorganizador do Hackaton do Uxgame – 2018.<br>
-                Elaboração e Construção do Game “UX Game” – 2018.<br>
-                Elaboração e Construção do Extrato Individual de Premiações – 2018.<br>
-                Ganhador do Pensa Gecap 5 em Parceria com Sudamar Cerqueira Albino – 2018.<br>
+                Elaboração e Manutenção do Talentos em Conexão - 2018 e 2019.<br>
+                Coorganizador do Hackaton do Uxgame - 2018.<br>
+                Elaboração e Construção do Game “UX Game” - 2018.<br>
+                Elaboração e Construção do Extrato Individual de Premiações - 2018.<br>
+                Ganhador do Pensa Gecap 5 em Parceria com Sudamar Cerqueira Albino - 2018.<br>
                 Premiado do Programa “Eu Faço a Diferença na T.I.” pela participação no Módulo Mobilização - 2018.<br>
-                Guru de Testes Automatizado para Mainframe – 2018.<br>
-                Elaboração e Construção Módulo Mobilização do Conexão – 2017.<br>
-                Elaboração e Construção Receita Interna de Atendimento e Transacional – 2016.<br>
-                Construção Receita Interna de Vendas – 2015.
+                Guru de Testes Automatizado para Mainframe - 2018.<br>
+                Elaboração e Construção Módulo Mobilização do Conexão - 2017.<br>
+                Elaboração e Construção Receita Interna de Atendimento e Transacional - 2016.<br>
+                Construção Receita Interna de Vendas - 2015.
             </p>
         </div>
     </div>`;
